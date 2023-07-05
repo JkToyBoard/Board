8b9d8d3ef100e2d26015aff5d6747173a4e27f25
LeeJeYoun <dlwpdbs1229@naver.com>
2023-07-05 10:02:18 +0900

    006-FEAT-DELETE-BOARD
    -> 게시글 삭제 기능 구현
    -> S3에 등록된 파일도 삭제

--- src/main/java/com/hidevelop/board/service/Impl/BoardServiceImpl.java
+++ src/main/java/com/hidevelop/board/service/Impl/BoardServiceImpl.java
@@ -20,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -33,14 +34,15 @@ public class BoardServiceImpl implements BoardService {
 
     /**
      * 게시판 글 등록
-     * @param files (이미지 파일)
-     * @param request (게시글 제목 , 내용)
+     *
+     * @param files    (이미지 파일)
+     * @param request  (게시글 제목 , 내용)
      * @param username (작성자)
      * @return
      */
-    public BoardDto.Response saveBoard(List<MultipartFile> files, BoardDto.Request request, String username){
+    public BoardDto.Response saveBoard(List<MultipartFile> files, BoardDto.Request request, String username) {
         User user = userRepository.findByUsername(username)
-                .orElseThrow( () -> new AuthenticationException(AuthErrorMessage.USER_NOT_FOUND));
+                .orElseThrow(() -> new AuthenticationException(AuthErrorMessage.USER_NOT_FOUND));
 
         List<String> images = s3ServiceImpl.uploadImage(files);
 
@@ -54,10 +56,11 @@ public class BoardServiceImpl implements BoardService {
 
     /**
      * 전체 게시글 조회 내림차순이라구~~~!
+     *
      * @param pageable (1페이지당 10개)
      * @return
      */
-    public Page<BoardDto.simpleBoard> readAllBoard(Pageable pageable){
+    public Page<BoardDto.simpleBoard> readAllBoard(Pageable pageable) {
         Page<Board> boards = boardRepository.findAll(pageable);
 
 
@@ -66,6 +69,7 @@ public class BoardServiceImpl implements BoardService {
 
     /**
      * 페이지 Entity를 필요한 자원만 있는 페이지 DTO로 쏙쏙
+     *
      * @param boards
      * @return
      */
@@ -86,6 +90,7 @@ public class BoardServiceImpl implements BoardService {
 
     /**
      * 게시글 하나 조회~!
+     *
      * @param boardId
      * @return
      */
@@ -102,11 +107,12 @@ public class BoardServiceImpl implements BoardService {
 
     /**
      * 게시글 수정
+     *
      * @param request
      * @param files
      * @return
      */
-    public BoardDto.Response updateBoard(BoardDto.UpdateRequest request, List<MultipartFile> files){
+    public BoardDto.Response updateBoard(BoardDto.UpdateRequest request, List<MultipartFile> files) {
         Board board = boardRepository.findById(request.getId())
                 .orElseThrow(() -> new ApplicationException(ApplicationErrorMessage.NOT_REGISTERED_BOARD));
 
@@ -117,4 +123,18 @@ public class BoardServiceImpl implements BoardService {
         Board resultBoard = boardRepository.save(board);
         return resultBoard.Of();
     }
+
+    /**
+     * 게시글 삭제
+     *
+     * @param boardId
+     */
+    public void deleteBoard(Long boardId) {
+        Board board = boardRepository.findById(boardId)
+                .orElseThrow(() -> new ApplicationException(ApplicationErrorMessage.NOT_REGISTERED_BOARD));
+
+        s3ServiceImpl.deleteImage(board);
+        boardRepository.deleteById(boardId);
+    }
+
 }
